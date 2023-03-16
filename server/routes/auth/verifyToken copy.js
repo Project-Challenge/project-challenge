2436fb13b3e414f5eb6a9526a232281681502166
Papayusen <KarolekB2002@gmail.com>
2023-03-16 23:03:32 +0100

    working on auth token

--- server/routes/auth/verifyToken copy.js
+++ server/routes/auth/verifyToken copy.js
@@ -5,15 +5,15 @@ const logger = require("../../utils/logger");
 
 router.get("/", async (req, res) => {
   try {
-    const token = req.body.token;
+    const token = req.headers["x-access-token"];
     if (!token) {
       return res.status(401).json({ error: "Access denied. No token provided." });
     }
     const decoded = await jwt.verify(token, process.env.SECRET_KEY);
     logger.info(`Valid token received for user ${decoded.username}`);
-    return res.send({ isValid: true, id: decoded.id, username: decoded.username });
-  } catch (error) {
-    logger.warn(`Error verifying token: ${error.message}`);
+    return res.send({ isValid: true, username: decoded.username });
+  } catch (err) {
+    logger.warn(`Error verifying token: ${err.message}`);
     return res.status(401).json({ error: "Access denied. Invalid token." });
   }
 });
