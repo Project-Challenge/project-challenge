73bf22972e20866440ce703502bd82e735980286
Szymon Ratkiewicz <ratkiewicz.szymon@gmail.com>
2023-04-19 10:15:35 +0200

    Update tasks.js

--- server/routes/tasks/tasks.js
+++ server/routes/tasks/tasks.js
@@ -27,7 +27,9 @@ router.get("/", async (req, res) => {
         startDate: { $lt: new Date() },
       }),
     };
-    const tasks = await TaskModel.find(query);
+    const tasks = await TaskModel.find(query)
+      .populate('author', 'username')
+      .populate('recipient', 'username');
     if (!tasks || tasks.length === 0) {
       logger.debug("No tasks found");
       return res.status(404).send({ error: "No tasks found" });
