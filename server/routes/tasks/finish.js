979634b3c9ef5f570db223ed68696f288c81f049
Karol-Bobryk <Karolekb2002@gmail.com>
2023-04-20 12:57:26 +0200

    changes to finish card button

--- server/routes/tasks/finish.js
+++ server/routes/tasks/finish.js
@@ -1,41 +1,41 @@
 const express = require("express");
 const router = express.Router();
 const logger = require("../../utils/logger");
-const TaskModel = require('../../models/tasks.js');
+const TaskModel = require("../../models/tasks.js");
 const joi = require("joi");
 
 router.post("/", async (req, res) => {
   try {
     const result = await validateTask(req.body);
-    if (result.error)  {
+    if (result.error) {
       logger.info(result.error.details[0].message);
-      return res.status(403).send({error: result.error.details[0].message});
+      return res.status(403).send({ error: result.error.details[0].message });
     }
     const task = await TaskModel.findByIdAndUpdate(
       req.body.id,
       {
         state: 2,
-        verifiedBy: "placeholder",
-        finishedDate: new Date()
+        verifiedBy: req.body.user._id,
+        finishedDate: new Date(),
       },
       { new: true }
     );
-    if (!task) {    
-      return res.status(404).send({error: "Task not found"});
+    if (!task) {
+      return res.status(404).send({ error: "Task not found" });
     }
     logger.info(`Task "${task._id}" set to finished`);
     res.send(task);
   } catch (err) {
     logger.error(err);
-    res.status(500).send({error: "Internal Server Error"});
+    res.status(500).send({ error: "Internal Server Error" });
   }
 });
 
 async function validateTask(user) {
   const schema = joi.object({
-    id: joi.string().required()
+    id: joi.string().required(),
   });
-  return schema.validate(user); 
+  return schema.validate(user);
 }
 
 module.exports = router;
