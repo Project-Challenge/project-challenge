a14750506f7789f11b0c1e3441c1f566bf0d093c
Karol-Bobryk <Karolekb2002@gmail.com>
2023-05-17 12:14:21 +0200

    restyled filters and search bar

--- client/src/pages/UserChallenges.jsx
+++ client/src/pages/UserChallenges.jsx
@@ -112,18 +112,6 @@ const UserChallenges = () => {
                 checked={isVerify}
                 onChange={handleRadioChange}
               />
-
-              <Form.Control
-                style={{ width: '10rem' }}
-                as='select'
-                onChange={handleSelectChange}>
-                <option value='-1'>State</option>
-                <option value='0'>New</option>
-                <option value='1'>Pending</option>
-                <option value='2'>Finished</option>
-              </Form.Control>
-            </div>
-            <div className='searchBox'>
               <Form.Control
                 style={{ width: '30rem' }}
                 className='loginControls'
@@ -132,6 +120,16 @@ const UserChallenges = () => {
                 value={searchTerm}
                 onChange={handleSearch}
               />
+              <Form.Control
+                style={{ width: '10rem' }}
+                as='select'
+                className='loginControls'
+                onChange={handleSelectChange}>
+                <option value='-1'>State</option>
+                <option value='0'>New</option>
+                <option value='1'>Pending</option>
+                <option value='2'>Finished</option>
+              </Form.Control>
             </div>
           </Form>
           {challenges ? (
